2bdfd5d5669d25d40dbd601edf578ce463a0373b
Vuanjun <victor@meepshop.com>
2026-02-10 19:55:36 +0800

    手機poster覆蓋

--- intro.js
+++ intro.js
@@ -27,7 +27,7 @@
   var startBtn = document.getElementById('intro-start-btn');
   var volumeBtn = document.getElementById('intro-volume-btn');
   var volumeBtnIcon = volumeBtn ? volumeBtn.querySelector('i') : null;
-// mobile 優化 02/10, 改個什麼覆蓋
+// mobile 優化 02/10, 改個什麼覆蓋, 手機poster覆蓋用
   var loadingStartTime = Date.now();
   var pauseIndex = 0;
   var hasUnmuted = false;
